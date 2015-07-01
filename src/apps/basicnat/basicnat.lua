12ad72cd9f8ddab54ee50449654d9e0d2313f74f
Andy Wingo <wingo@igalia.com>
2015-07-01 11:59:02 +0200

    Update for latest pfmatch language sketches

--- src/apps/basicnat/basicnat.lua
+++ src/apps/basicnat/basicnat.lua
@@ -17,22 +17,6 @@ local function str_ip_to_uint32(ip)
    return bytes_to_uint32(tonumber(a), tonumber(b), tonumber(c), tonumber(d))
 end
 
-local function subst(str, values)
-   local out, pos = '', 1
-   while true do
-      local before, after = str:match('()%$[%w_]+()', pos)
-      if not before then return out..str:sub(pos) end
-      out = out..str:sub(pos, before - 1)
-      local var = str:sub(before + 1, after - 1)
-      local val = values[var]
-      print (before, after, var, val)
-      if not val then error('var not found: '..var) end
-      out = out..val
-      pos = after
-   end
-   return out
-end
-
 local function uint32_to_bytes(u)
    local a = bit.rshift(u, 24)
    local b = bit.band(bit.rshift(u, 16), 0xff)
@@ -53,12 +37,10 @@ local function ipv4_checksum(data, ip_base)
    local IHL = bit.rshift(data[ip_base], 4)
    local len = IHL * 4
    local checksum = 0
-   for i = ip_base, ip_base + len, 2 do
-      if i ~= ip_base + 10 then -- The checksum bytes are assumed to be 0
-         checksum = checksum + data[i] * 0x100 + data[i+1]
-      end
-   end
-   return csum_carry_and_not(checksum)
+   -- Checksum bytes are 0 while computing checksum.
+   data[10] = 0
+   data[11] = 0
+   return checksum.ipsum(data + ip_base, len - ip_base, 0)
 end
 
 local function tcp_checksum(data, ip_base, tcp_base, tcp_len)
@@ -125,85 +107,74 @@ local function set_dst_ip(data, ip_base, ip)
    fix_ip_checksums(data, ip_base)
 end
 
--- For packets outbound from the
--- private IP, the source IP address and related fields such as IP,
--- TCP, UDP and ICMP header checksums are translated. For inbound
--- packets, the destination IP address and the checksums as listed above
--- are translated.
-
--- FIXME: Would be nice to have &ip src as an addressable.
--- todo: add comment syntax, "else"
-local dispatch_template = [[
-(forward, drop,
- incoming_tcp, incoming_udp, incoming_other,
- outgoing_tcp, outgoing_udp, outgoing_other) => {
-   not ip => forward
-   ip[6] & 0x4 = 1 => drop
-   ip src $external_ip => {
-      tcp => incoming_tcp(&ip[0], &tcp[0])
-      udp => incoming_udp(&ip[0], &udp[0])
-      _ => incoming_other(&ip[0])
-   }
-   ip dst $internal_ip => {
-      tcp => outgoing_tcp(&ip[0], &tcp[0])
-      udp => outgoing_udp(&ip[0], &udp[0])
-      _ => outgoing_other(&ip[0])
-   }
-   _ => drop
-}]]
-
-local function forward(data, len)
-   return len
-end
-
-local function drop(data, len)
-   -- Could truncate packet here and overwrite with ICMP error if
-   -- wanted.
-   return false
-end
-
-local function make_dispatcher(conf)
-   local external_ip = str_ip_to_uint32(conf.external_ip)
-   local internal_ip = str_ip_to_uint32(conf.internal_ip)
+local function make_handlers(external_ip, internal_ip)
    -- FIXME: Verify IP header length within packet length?  What about
    -- payload length?
-   local function incoming_tcp(data, len, ip_base, tcp_base)
+   local handlers = {}
+   function handlers.forward(data, len)
+      return len
+   end
+   function handlers.drop(data, len)
+      -- Could truncate packet here and overwrite with ICMP error if
+      -- wanted.
+      return nil
+   end
+   function handlers.incoming_tcp(data, len, ip_base, tcp_base)
       set_src_ip(data, ip_base, internal_ip)
       fix_tcp_checksums(data, tcp_base, len - tcp_base)
       return len
    end
-   local function incoming_udp(data, len, ip_base, udp_base)
+   function handlers.incoming_udp(data, len, ip_base, udp_base)
       set_src_ip(data, ip_base, internal_ip)
       fix_udp_checksums(data, udp_base, len - udp_base)
       return len
    end
-   local function incoming_other(data, len, ip_base, payload_base)
+   function handlers.incoming_other(data, len, ip_base, payload_base)
       set_src_ip(data, ip_base, internal_ip)
       return len
    end
-   local function outgoing_tcp(data, len, ip_base, tcp_base)
+   function handlers.outgoing_tcp(data, len, ip_base, tcp_base)
       set_dst_ip(data, ip_base, external_ip)
       fix_tcp_checksums(data, tcp_base, len - tcp_base)
       return len
    end
-   local function outgoing_udp(data, len, ip_base, udp_base)
+   function handlers.outgoing_udp(data, len, ip_base, udp_base)
       set_dst_ip(data, ip_base, external_ip)
       fix_udp_checksums(data, udp_base, len - udp_base)
       return len
    end
-   local function outgoing_other(data, len, ip_base)
+   function handlers.outgoing_other(data, len, ip_base)
       set_dst_ip(data, ip_base, external_ip)
       return len
    end
-   return pf.dispatch.compile(subst(dispatch_template, conf))(
-      forward, drop,
-      incoming_tcp, incoming_udp, incoming_other,
-      outgoing_tcp, outgoing_udp, outgoing_other)
+   return handlers
 end
 
 function BasicNAT:new (conf)
-   local c = {dispatch = make_dispatcher(conf)}
-   return setmetatable(c, {__index=BasicNAT})
+   local app = {}
+   app.handlers = make_handlers(str_ip_to_uint32(conf.external_ip)
+                                str_ip_to_uint32(conf.internal_ip))
+   -- For packets outbound from the private IP, the source IP address
+   -- and related fields such as IP, TCP, UDP and ICMP header checksums
+   -- are translated. For inbound packets, the destination IP address
+   -- and the checksums as listed above are translated.
+   app.match = pf.match.compile([[match {
+      not ip => forward
+      -- Drop fragmented packets.
+      ip[6:2] & 0x3fff != 0 => drop
+      ip src $external_ip => {
+         tcp => incoming_tcp(&ip[0], &tcp[0])
+         udp => incoming_udp(&ip[0], &udp[0])
+         otherwise => incoming_other(&ip[0])
+      }
+      ip dst $internal_ip => {
+         tcp => outgoing_tcp(&ip[0], &tcp[0])
+         udp => outgoing_udp(&ip[0], &udp[0])
+         otherwise => outgoing_other(&ip[0])
+      }
+      otherwise => drop
+   }]], {subst=conf})
+   return setmetatable(app, {__index=BasicNAT})
 end
 
 function BasicNAT:push ()

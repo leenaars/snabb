aa63ee3a4f1917ae8657e7abe5092dbec0eefe9a
Adrián Pérez de Castro <aperez@igalia.com>
2016-02-08 20:04:47 +0200

    Implement utility functions for doubly linked lists
    This adds new apps.lwaftr.lwutil.linked_list_*() which can be used to
    manipulate circular doubly linked lists of Lua tables. The functions
    add the keys "_linked_list_next" and "_linked_list_prev" to the tables
    added to the list, so no additional tables are created internally for
    housekeeping. The "handle" to a list is a variable with a reference to
    the list's first item. Functions which modify the list may return a
    changed list "handle". Quick usage example:
    
      local list = nil  -- Empty list
      list = linked_list_append(list, { message = "Hello" })
      list = linked_list_append(list, { message = "World" })
      while not linked_list_empty(list) do
        local item = linked_list_first(list)
        list = linked_list_remove(list, item)
        print(item.message)
      end

--- src/apps/lwaftr/lwutil.lua
+++ src/apps/lwaftr/lwutil.lua
@@ -69,3 +69,160 @@ end
 function is_ipv4(pkt)
    return rd16(pkt.data + o_ethernet_ethertype) == n_ethertype_ipv4
 end
+
+
+-- The following functions make any table an element of a linked list:
+--
+--  * Members "_linked_list_next", and "_linked_list_prev" are used to
+--    keep the links between items -- do not modify them manually.
+--
+--  * Functions which modify the list return the (possibly updated)
+--    list first item. Make sure to assign the return value from them
+--    to your reference to the "list head item".
+--
+function linked_list_append(list_head, list_item)
+   if list_head then
+      list_item._linked_list_next = list_head
+      list_item._linked_list_prev = list_head._linked_list_prev
+      list_item._linked_list_prev._linked_list_next = list_item
+      list_head._linked_list_prev = list_item
+   else
+      -- Empty list
+      list_item._linked_list_next = list_item
+      list_item._linked_list_prev = list_item
+      list_head = list_item
+   end
+   return list_head
+end
+
+function linked_list_remove(list_head, list_item)
+   if list_item == list_item._linked_list_prev then
+      -- There's only one element in the list.
+      list_head = nil
+   else
+      list_item._linked_list_prev._linked_list_next = list_item._linked_list_next
+      list_item._linked_list_next._linked_list_prev = list_item._linked_list_prev
+      if list_item == list_head then
+         list_head = list_item._linked_list_next
+      end
+   end
+
+   list_item._linked_list_next = nil
+   list_item._linked_list_prev = nil
+   return list_head
+end
+
+function linked_list_first(list_head)
+   return list_head
+end
+
+function linked_list_last(list_head)
+   return list_head._linked_list_prev
+end
+
+function linked_list_is_empty(list_head)
+   return not list_head
+end
+
+function linked_list_next(list_head, list_item)
+   if list_item == linked_list_last(list_head) then
+      return nil
+   else
+      return list_item._linked_list_next
+   end
+end
+
+function linked_list_prev(list_head, list_item)
+   if list_item == linked_list_first(list_head) then
+      return nil
+   else
+      return list_item._linked_list_prev
+   end
+end
+
+
+function selftest()
+   print("linked_list_* functions")
+   do
+      local function check_list_links(l)
+         if l then
+            local item, last = linked_list_first(l), linked_list_last(l)
+            while item do
+               assert(item._linked_list_next)
+               assert(item._linked_list_next._linked_list_prev == item)
+               assert(item._linked_list_prev)
+               assert(item._linked_list_prev._linked_list_next == item)
+               item = linked_list_next(l, item)
+            end
+         end
+         return l
+      end
+
+      print("  append (empty)")
+      local l = nil
+      l = assert(check_list_links(linked_list_append(l, { item=1 })))
+      assert(not linked_list_is_empty(l))
+      assert(l == linked_list_last(l))
+      assert(l.item == 1)
+
+      print("  append (non-empty)")
+      local i = { item=2 }
+      l = assert(check_list_links(linked_list_append(l, i)))
+      assert(not linked_list_is_empty(l))
+      assert(i == linked_list_last(l))
+
+      print("  remove (last item)")
+      l = assert(check_list_links(linked_list_remove(l, i)))
+      assert(linked_list_last(l) == linked_list_first(l))
+
+      print("  remove (last item, leave empty)")
+      l = linked_list_remove(l, linked_list_last(l))
+      assert(linked_list_is_empty(l))
+      assert(l == nil)
+
+      print("  remove (all)")
+      for i, _ in ipairs { 1, 2, 3, 4 } do
+         l = assert(check_list_links(linked_list_append(l, { item=i })))
+      end
+      i = 0
+      while not linked_list_is_empty(l) do
+         l = check_list_links(linked_list_remove(l, linked_list_last(l)))
+         i = i + 1
+      end
+      assert(i == 4)
+      assert(l == nil)
+
+      print("  remove (first item)")
+      for i, _ in ipairs { 1, 2, 3, 4 } do
+         l = assert(check_list_links(linked_list_append(l, { item=i })))
+      end
+      l = assert(check_list_links(linked_list_remove(l, linked_list_first(l))))
+      i = 0
+      while not linked_list_is_empty(l) do
+         l = linked_list_remove(l, linked_list_last(l))
+         i = i + 1
+      end
+      assert(i == 3)
+      assert(l == nil)
+
+      print("  remove (first item, one-item list)")
+      l = assert(check_list_links(linked_list_append(l, { item=1 })))
+      l = linked_list_remove(l, linked_list_first(l))
+      assert(linked_list_is_empty(l))
+      assert(l == nil)
+
+      print("  remove (item in the middle)")
+      l = assert(check_list_links(linked_list_append(l, { item=1 })))
+      i = { item = 2 }
+      l = assert(check_list_links(linked_list_append(l, i)))
+      l = assert(check_list_links(linked_list_append(l, { item=3 })))
+      l = assert(check_list_links(linked_list_remove(l, i)))
+      i = 0
+      while not linked_list_is_empty(l) do
+         l = linked_list_remove(l, linked_list_last(l))
+         i = i + 1
+      end
+      assert(i == 2)
+      assert(l == nil)
+   end
+end
